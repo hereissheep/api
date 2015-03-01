09c3395a293a64aa7a3824ee23bebf669c538503
Ítalo Lelis de Vietro <italolelis@gmail.com>
2015-03-01 01:34:43 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hereissheep
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/CategoriesController.js
@@ -0,0 +1,11 @@
+/**
+ * CategoriesController
+ *
+ * @description :: Server-side logic for managing categories
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
